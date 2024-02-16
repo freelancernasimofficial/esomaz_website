8d091cc35570a59b164cb0306a08eea3d97862b1
freelancernasimofficial <freelancernasim@gmail.com>
2024-02-17 03:53:23 +0600

    update

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -4,6 +4,7 @@ import IconRss from "@/components/icons/IconRss";
 import IconSchoolOutline from "@/components/icons/IconSchoolOutline";
 import PostCard from "@/components/post/PostCard";
 import PostForm from "@/components/post/PostForm";
+import UserIntro from "@/components/user/UserIntro";
 import Model from "@/model/Model";
 import Image from "next/image";
 import Link from "next/link";
@@ -16,39 +17,16 @@ type Props = {
 };
 
 export default async function page({ params }: Props) {
+  const [user] = await Model.prepare(
+    `SELECT id FROM Users WHERE username=? OR uuId=?`,
+    [params.userId, params.userId],
+  );
+
   return (
     <div className='centerCard  overflow-hidden mt-6'>
       <div className='md:flex w-full'>
         <div className='lg:w-[450px] md:w-[350px] w-full md:pr-6'>
-          <div className='w-full p-4 rounded-lg bg-white shadow'>
-            <div className='mb-4 flex justify-between'>
-              <h2 className='font-bold text-xl'>Intro</h2>
-              <Link className='text-primary-main  text-sm2' href='#'>
-                Edit
-              </Link>
-            </div>
-
-            <div className='text-sm2 flex items-center'>
-              <IconLocationOutline className='w-6 h-6 mr-1 shrink-0' />{" "}
-              <span className='mr-2'>Lives In</span>{" "}
-              <span className='font-semibold'>Cairo , Eygept</span>
-            </div>
-            <div className='text-sm2 flex items-center mt-4'>
-              <IconSchoolOutline className='w-6 h-6 mr-2 shrink-0' />{" "}
-              <span className='mr-2'>Studied at</span>{" "}
-              <span className='font-semibold'>University of Turkey</span>
-            </div>
-            <div className='text-sm2 flex items-center mt-4'>
-              <IconBriefcase className='w-5 h-5 mr-2 ml-0.5 shrink-0' />{" "}
-              <span className='mr-2'>Works at</span>{" "}
-              <span className='font-semibold'>Envato Market</span>
-            </div>
-            <div className='text-sm2 flex items-center mt-4'>
-              <IconRss className='w-5 h-5 mr-2 ml-0.5 shrink-0' />{" "}
-              <span className='mr-2'>Followed by</span>{" "}
-              <span className='font-semibold'>1,205,200 People</span>
-            </div>
-          </div>
+          <UserIntro userId={user?.id} />
           <div className='w-full p-2 mt-6 rounded-lg bg-white shadow'>
             <div className='px-3 pt-1.5 flex justify-between'>
               <h2 className='font-bold text-xl'>Friends</h2>
---
+++ src/components/user/UserIntro.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import React from "react";
+import IconBriefcase from "../icons/IconBriefcase";
+import IconLocationOutline from "../icons/IconLocationOutline";
+import IconRss from "../icons/IconRss";
+import IconSchoolOutline from "../icons/IconSchoolOutline";
+import Model from "@/model/Model";
+
+type Props = {
+  userId: number;
+};
+
+export default async function UserIntro({ userId }: Props) {
+  const [userInfos] = await Model.query(
+    `SELECT *,(SELECT name FROM Countries WHERE id=UF.countryId) AS country,(SELECT COUNT(*) FROM Followers FL WHERE FL.userId=UF.userId) AS totalFollowers FROM UserInfos UF WHERE UF.userId=${userId}`,
+  );
+
+  return (
+    <div className='w-full p-4 rounded-lg bg-white shadow'>
+      <div className='mb-1 flex justify-between'>
+        <h2 className='font-bold text-xl'>Intro</h2>
+
+        <Link className='text-primary-main  text-sm2' href='#'>
+          Edit
+        </Link>
+      </div>
+
+      <div className='mb-3 text-sm2 text-gray-600'>{userInfos?.shortBio}</div>
+
+      <div className='text-sm2 flex items-center'>
+        <IconLocationOutline className='w-6 h-6 mr-1 shrink-0' />{" "}
+        <span className='mr-2'>Lives In</span>{" "}
+        <span className='font-semibold'>
+          {userInfos?.city +
+            ", " +
+            userInfos?.state +
+            ", " +
+            userInfos?.country}
+        </span>
+      </div>
+      <div className='text-sm2 flex items-center mt-4'>
+        <IconSchoolOutline className='w-6 h-6 mr-2 shrink-0' />{" "}
+        <span className='mr-2'>Studied at</span>{" "}
+        <span className='font-semibold'>
+          {userInfos?.studyLevel + ", " + userInfos?.instituteName}
+        </span>
+      </div>
+      <div className='text-sm2 flex items-center mt-4'>
+        <IconBriefcase className='w-5 h-5 mr-2 ml-0.5 shrink-0' />{" "}
+        <span className='mr-2'>Works at</span>{" "}
+        <span className='font-semibold'>
+          {" "}
+          {userInfos?.workDesignation + ", " + userInfos?.workingCompany}
+        </span>
+      </div>
+      <div className='text-sm2 flex items-center mt-4'>
+        <IconRss className='w-5 h-5 mr-2 ml-0.5 shrink-0' />{" "}
+        <span className='mr-2'>Followed by</span>{" "}
+        <span className='font-semibold'>
+          {userInfos?.totalFollowers} People
+        </span>
+      </div>
+    </div>
+  );
+}
