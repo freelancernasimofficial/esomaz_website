8309bb9d7630cabc7652f7c85fa45802d4356d54
freelancernasimofficial <freelancernasim@gmail.com>
2024-03-10 02:53:08 +0600

    update

--- src/actions/deleteCommentAction.ts
+++ src/actions/deleteCommentAction.ts
@@ -1,7 +1,16 @@
 "use server";
+
+import Model from "@/model/Model";
+import { revalidatePath } from "next/cache";
+
 export default async function deleteCommentAction(
   commentId: number,
   formData: any,
 ) {
-  console.log(commentId);
+  if (commentId) {
+    //delete comment
+    await Model.prepare("DELETE FROM Comments WHERE id=?", [commentId]);
+
+    revalidatePath("/");
+  }
 }
