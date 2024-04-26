fd8c28af75a9cb487d1f9d3c38a89265c47f9c05
freelancernasimofficial <freelancernasim@gmail.com>
2024-04-26 07:33:42 +0600

    update

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import IconX from "@/components/icons/IconX";
+import React from "react";
+
+type Props = {};
+
+export default function notfound({}: Props) {
+  return (
+    <div className='centerCardMobile bg-white h-40 shadow flex justify-center items-center mt-10'>
+      <div className='flex flex-col items-center justify-center'>
+        {" "}
+        <div className='w-12 h-12 rounded-full overflow-hidden flex items-center justify-center bg-red-400 text-white mb-4'>
+          {" "}
+          <IconX className='w-10 h-10' />
+        </div>
+        <h1 className='text-xl font-medium'>404 | Not Found</h1>
+      </div>
+    </div>
+  );
+}
