fae5b17ce8b0c4d654224a60b1a2c2a4f6961aaf
freelancernasimofficial <freelancernasim@gmail.com>
2024-04-26 08:02:28 +0600

    update

--- src/components/home/RightSidebar.tsx
+++ src/components/home/RightSidebar.tsx
@@ -1,43 +1,42 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import getPeopleYouMayKnowAction from "@/actions/getPeopleYouMayKnowAction";
+import Avatar from "../user/Avatar";
+import getFullName from "@/library/getFullName";
+import getRelativeTime from "@/library/getRelativeTime";
+import getUsername from "@/library/getUsername";
 
 type Props = {};
 
-export default function RightSidebar({}: Props) {
+export default async function RightSidebar({}: Props) {
+  const people = await getPeopleYouMayKnowAction();
+
   return (
     <div className='hidden lg:flex max-w-96 w-full full-height sticky top-20  flex-col pl-6'>
       <div className='bg-white shadow p-4 rounded-lg overflow-hidden '>
         <div className='flex justify-between items-center pb-2'>
-          <h2 className='font-semibold text-lg'>Active Friends</h2>
-          <Link href='#' className='text-sm2 text-primary-main'>
-            See All
-          </Link>
+          <h2 className='font-semibold text-lg'>People You May Know</h2>
         </div>
         <div className='flex-1 w-full h-full overflow-y-scroll pb-10'>
-          {[...Array(15)].map((item, index) => {
+          {people?.map((user: any, index: number) => {
             return (
-              <div key={index.toString()} className='flex justify-between mb-4'>
+              <div key={user?.uuId} className='flex justify-between mb-4'>
                 <div className='flex'>
                   <div className='w-9 h-9 overflow-hidden shrink-0 rounded-full'>
-                    <Image
-                      className='w-full h-full'
-                      height={40}
-                      width={40}
-                      alt='user avatar'
-                      src={`/images/static/avatars/avatar-1.jpg`}
-                    />
+                    <Avatar user={user} />
                   </div>
                   <div className='ml-2'>
-                    <Link href='/user' className='block'>
-                      <h4 className='font-medium text-sm2'>Md Nasim</h4>
+                    <Link href={`/user/${getUsername(user)}`} className='block'>
+                      <h4 className='font-medium text-sm2'>
+                        {getFullName(user)}
+                      </h4>
                     </Link>
                     <span className='block text-sm5 text-gray-500 leading-3'>
-                      Active Now
+                      Joined {getRelativeTime(user?.createdAt)}
                     </span>
                   </div>
                 </div>
-                <button className='btn btn-primary-transparent'>Chat</button>
               </div>
             );
           })}
