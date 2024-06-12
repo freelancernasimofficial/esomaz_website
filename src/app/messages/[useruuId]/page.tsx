1c53e4bd3e8a59ecd3f0797f8dd7163b5ecb2bf4
freelancernasimofficial <freelancernasim@gmail.com>
2024-06-13 04:38:50 +0600

    update

--- src/app/messages/[useruuId]/page.tsx
+++ src/app/messages/[useruuId]/page.tsx
@@ -52,19 +52,22 @@ export default async function page({ params }: Props) {
         })}
       </div>
 
-      <div className='bg-gray-300 w-full flex items-center justify-between fixed bottom-0'>
-        <textarea
-          className='w-full rounded-none flex-1 h-full focus-visible:outline-none bg-transparent'
-          placeholder='Enter Message'
-          cols={10}
-          rows={1}
-        ></textarea>
-        <button
-          className='bg-blue-700 text-white flex items-center justify-center w-16 h-12 active:rounded active:scale-75 
+      <div>
+        <div className='h-12 w-full'></div>
+        <div className='bg-gray-300 w-full h-12 flex items-center justify-between fixed bottom-0'>
+          <textarea
+            className='w-full rounded-none flex-1 h-full focus-visible:outline-none bg-transparent'
+            placeholder='Enter Message'
+            cols={10}
+            rows={1}
+          ></textarea>
+          <button
+            className='bg-blue-700 text-white flex items-center justify-center w-16 h-full active:rounded active:scale-75 
          duration-300'
-        >
-          <IconSendCircle className='w-7 h-7' />
-        </button>
+          >
+            <IconSendCircle className='w-7 h-7' />
+          </button>
+        </div>
       </div>
     </section>
   );
