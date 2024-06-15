3399b2040217152db763aef9203b4b57f2c7b87c
freelancernasimofficial <freelancernasim@gmail.com>
2024-06-15 18:31:53 +0600

    update

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -3,6 +3,7 @@ import PostForm from "@/components/post/PostForm";
 import React from "react";
 import UserPosts from "@/components/user/UserPosts";
 import { getSingleUserByuuId } from "@/actions/user/userActions";
+import auth from "@/actions/auth/auth";
 
 type Props = {
   params: {
@@ -12,12 +13,14 @@ type Props = {
 
 export default async function page({ params }: Props) {
   const user = await getSingleUserByuuId(params?.userId);
-
+  const currentUser = await auth();
   return (
     <React.Fragment>
-      <div className='mt-4 md:mt-0'>
-        <PostForm />
-      </div>
+      {currentUser?.id === user?.id ? (
+        <div className='mt-4 md:mt-0'>
+          <PostForm />
+        </div>
+      ) : null}
       <UserPosts user={user} />
     </React.Fragment>
   );
